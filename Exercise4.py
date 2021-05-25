6fab4d1ddf29a4825b28bb2656acb4323b1aedd8
= <=>
2021-05-25 16:55:16 +0300

    Exercise 4

---
+++ Exercise4.py
@@ -0,0 +1,17 @@
+'''
+4. Write a Python program to sort a list of dictionaries using Lambda.
+Original list of dictionaries :
+[{'make': 'Nokia', 'model': 216, 'color': 'Black'}, {'make': 'Mi Max', 'model': '2', 'color': 'Gold'}, {'make': 'Samsung', 'model': 7, 'color': 'Blue'}]
+Sorting the List of dictionaries :
+[{'make': 'Nokia', 'model': 216, 'color': 'Black'}, {'make': 'Samsung', 'model': 7, 'color': 'Blue'}, {'make': 'Mi Max', 'model': '2', 'color': 'Gold'}]
+
+'''
+
+dict1 = [{'make': 'Nokia', 'model': 216, 'color': 'Black'}, {'make': 'Mi Max', 'model': '2', 'color': 'Gold'}, {'make': 'Samsung', 'model': 7, 'color': 'Blue'}]
+
+print(dict1)
+
+sorted_dict = sorted(dict1,key = lambda x : x["color"])
+
+print(sorted_dict)
+
