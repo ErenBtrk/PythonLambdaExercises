f4d730774f3b273c3871d4a43b591be15d160cbd
= <=>
2021-05-26 15:05:55 +0300

    Exercise 28

---
+++ Exercise28.py
@@ -0,0 +1,15 @@
+'''
+28. Write a Python program to sort a given list of lists by length and value using lambda.
+Original list:
+[[2], [0], [1, 3], [0, 7], [9, 11], [13, 15, 17]]
+Sort the list of lists by length and value:
+[[0], [2], [0, 7], [1, 3], [9, 11], [13, 15, 17]]
+
+'''
+
+def sortSublist(list):
+    return sorted(list,key = lambda x : (len(x),x))
+
+list1 = [[2], [0], [1, 3], [0, 7], [9, 11], [13, 15, 17]]
+
+print(sortSublist(list1))
